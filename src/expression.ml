26999de20131278a8b1807760db52086446869d5
Mário <mjp.pereira@fct.unl.pt>
2020-06-12 22:57:30 +0100

    Added support for tuples in type declaration.

--- src/expression.ml
+++ src/expression.ml
@@ -54,14 +54,12 @@ let rec core_type O.{ptyp_desc; ptyp_loc; _} = match ptyp_desc with
       PTtyapp (longident ~id_loc:(T.location loc) txt, args)
   | O.Ptyp_arrow (Nolabel, cty1, cty2) ->
       PTarrow (core_type cty1, core_type cty2)
-  | O.Ptyp_arrow (Labelled _, cty1, cty2) ->
-      ignore (cty1);
-      ignore (cty2);
+  | O.Ptyp_arrow (Labelled _, _, _) ->
       assert false (* TODO *)
-  | O.Ptyp_arrow (Optional _, cty1, cty2) ->
-      ignore (cty1);
-      ignore (cty2);
+  | O.Ptyp_arrow (Optional _, _, _) ->
       assert false (* TODO *)
+  | O.Ptyp_tuple cty_list ->
+      PTtuple (List.map core_type cty_list)
   | _ -> assert false (* TODO *)
 
 let binder_of_pattern O.{ppat_desc; ppat_loc; _} = match ppat_desc with
@@ -130,7 +128,7 @@ let rec expression Uast.{spexp_desc = p_desc; spexp_loc; _} =
         let id, binder_expr = s_value_binding svb in
         let expr = expression expr in
         Elet (id, false, Expr.RKnone, binder_expr, expr)
-    | Uast.Sexp_let _ -> assert false (* TODO *)
+    | Uast.Sexp_let (Recursive, _, _) -> assert false (* TODO *)
     | Uast.Sexp_fun (Nolabel, None, pat, expr_body, spec) ->
         let ret = T.mk_pattern Pwild in
         let expr_body = expression expr_body in
