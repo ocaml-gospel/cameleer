246c6e2dedf2d05fdaf802f82d0bacc9501e57ab
Mario Pereira <mjp.pereira@fct.unl.pt>
2020-09-22 13:05:24 +0100

    Ephemeral queue: proof of transfer completed! :D Yahooo !!!
    Tricky part: specification of the `pop` function. We need to be extra careful
    and explicitly use `old q.view` in the `Raises` clause.

--- tests/ephemeral_queue.ml
+++ tests/ephemeral_queue.ml
@@ -20,11 +20,21 @@ let push x q =
   q.size <- q.size + 1;
   q.view <- q.view @ (x :: [])
 (*@ push x q
-      ensures q.size = (old q.size) + 1 *)
+      ensures q.size = (old q.size) + 1
+      ensures q.view = (old q.view) @ (x :: []) *)
 
 let [@logic] is_empty_list = function
   | [] -> true
   | _ -> false
+
+let [@ghost] head_list = function
+  | [] -> assert false
+  | x :: _ -> x
+(*@ r = head_list param
+      requires param <> []
+      ensures  match param with
+               | [] -> false
+               | y :: _ -> r = y *)
 
 let pop q =
   if is_empty_list q.front then begin
@@ -38,19 +48,28 @@ let pop q =
       q.view  <- (match q.view with [] -> assert false | _ :: ff -> ff);
       x
 (*@ x = pop q
-      raises  Not_found -> is_empty_list q.view
+      raises  Not_found -> is_empty_list (old q).view (* SUPER IMPORTANT ! *)
       ensures q.size + 1 = (old q).size
       ensures x :: q.view = (old q).view *)
 
 let is_empty q =
   q.size = 0
 (*@ b = is_empty q
-      ensures b <-> q.view = [] *)
+      ensures b <-> is_empty_list q.view *)
 
 let transfer q1 q2 =
+  let [@ghost] todo_view = ref [] in
   while not (is_empty q1) do
+    (*@ variant   q1.size *)
+    (*@ invariant length q1.view = q1.size /\ length q2.view = q2.size *)
+    (*@ invariant q1.view = q1.front @ List.rev q1.rear /\
+                  q2.view = q2.front @ List.rev q2.rear *)
+    (*@ invariant old q1.view = !todo_view @ q1.view *)
+    (*@ invariant q2.view = (old q2.view) @ !todo_view *)
+    todo_view := !todo_view @ (head_list q1.view :: []);
     push (pop q1) q2
-    (*@ invariant true *)
   done
 (*@ transfer q1 q2
-      raises Not_found -> false *)
+      raises  Not_found -> false
+      ensures q1.view = []
+      ensures q2.view = (old q2.view) @ (old q1.view) *)
