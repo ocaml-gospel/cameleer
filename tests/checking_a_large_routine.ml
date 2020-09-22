638a19334ed0f4a0f855a50f89ad169aae6800c0
Mario Pereira <mjp.pereira@fct.unl.pt>
2020-09-22 16:35:17 +0100

    Cameleer meets history: "checking a large routine", by A. Turing.

---
+++ tests/checking_a_large_routine.ml
@@ -0,0 +1,40 @@
+let [@logic] [@ghost] rec fact x =
+  if x = 0 then 1
+  else x * fact (x - 1)
+(*@ r = fact x
+      requires x >= 0
+      variant  x *)
+
+let routine n =
+  let r = ref 0 in
+  let u = ref 1 in
+  while !r < n do
+    (*@ invariant 0 <= !r <= n /\ !u = fact !r *)
+    (*@ variant   n - !r *)
+    let s = ref 1 in
+    let v = !u in
+    while !s <= !r do
+      (*@ invariant 1 <= !s <= !r + 1 /\ !u = !s * fact !r *)
+      (*@ variant !r - !s *)
+      u := !u + v;
+      s := !s + 1
+    done;
+    r := !r + 1
+  done;
+  !u
+(*@ r = routine n
+      requires n >= 0
+      ensures  r = fact n *)
+
+let routine2 n =
+  let u = ref 1 in
+  for r = 0 to n-1 do (*@ invariant !u = fact r *)
+    let v = !u in
+    for s = 1 to r do (*@ invariant !u = s * fact r *)
+      u := !u + v
+    done
+  done;
+  !u
+(*@ r = routine2 n
+      requires n >= 0
+      ensures  r = fact n *)
