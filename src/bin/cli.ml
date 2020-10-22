59f8a8153325d389b21ba93fe5c6183e02abb226
Mario Pereira <mjp.pereira@fct.unl.pt>
2020-10-22 13:51:27 +0100

    Command line interface for cameleer.

---
+++ src/bin/cli.ml
@@ -0,0 +1,27 @@
+open Format
+
+let fname = ref None
+
+let path = Queue.create ()
+
+let spec = ["-L", Arg.String (fun s -> Queue.add s path),
+            "add <dir> to the search path"]
+
+let usage_msg =
+  sprintf "%s <file>.ml\n\
+           Verify OCaml program\n" Sys.argv.(0)
+
+let usage () = Arg.usage spec usage_msg; exit 1
+
+let set_file f = match !fname with
+  | None when Filename.check_suffix f ".ml" -> fname := Some f
+  | _ -> usage ()
+
+let () = Arg.parse spec set_file usage_msg
+
+let fname = match !fname with None -> usage () | Some f -> f
+
+let path = Queue.fold (fun acc s -> sprintf "-L %s %s" s acc) "" path
+
+let _ =
+  Sys.command (sprintf "why3ide %s %s" fname path)
