3a921bd6aa054d34f1c156b5dac64b83f15a0f52
Mário Pereira <mjp.pereira@fct.unl.pt>
2020-12-06 15:04:47 +0000

    Applicative queue example adapted to record binders.

--- examples/applicative_queue.ml
+++ examples/applicative_queue.ml
@@ -9,21 +9,21 @@ let empty = { self = [], []; view = [] }
 (*@ t = empty
       ensures t.view = [] *)
 
-let [@logic] is_empty (q: 'a t) = match q.self with
+let [@logic] is_empty {self; _} = match self with
   | [], _ -> true
   | _ -> false
 (*@ b = is_empty q
       ensures b <-> q.view = [] *)
 
-let add queue elt = match queue.self with
+let add elt {self; view} = match self with
   | [], [] ->
       { self = [elt], []; view = [elt] }
   | prefix, xiffus ->
-      { self = prefix, elt :: xiffus; view = queue.view @ [elt] }
-(*@ r = add queue elt
+      { self = prefix, elt :: xiffus; view = view @ [elt] }
+(*@ r = add elt queue
       ensures r.view = queue.view @ (elt :: []) *)
 
-let head (param: 'a t) = match param.self with
+let head {self; _} = match self with
   | head :: _, _-> head
   | [], _ -> raise Not_found
 (*@ x = head param
@@ -33,15 +33,15 @@ let head (param: 'a t) = match param.self with
 let [@logic] [@ghost] tail_list = function
   | [] -> assert false
   | _ :: l -> l
-(*@ r = tail_list param
-      requires param <> []
-      ensures  match param with [] -> false | _ :: l -> r = l *)
+(*@ r = tail_list q
+      requires q <> []
+      ensures  match q with [] -> false | _ :: l -> r = l *)
 
-let tail t = match t.self with
+let tail {self; view} = match self with
   | [_], xiffus ->
-      { self = List.rev xiffus, []; view = tail_list t.view }
+      { self = List.rev xiffus, []; view = tail_list view }
   | _ :: prefix, xiffus ->
-      { self = prefix, xiffus; view = tail_list t.view }
+      { self = prefix, xiffus; view = tail_list view }
   | [], _ -> raise Not_found
 (*@ r = tail t
       raises  Not_found -> is_empty t
