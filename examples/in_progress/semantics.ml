ed47ca7f8295ad150eadb3e7a52f0fae6075915d
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-01-05 16:18:27 +0000

    Arith compiler: verified compilation of addition operations.

--- examples/in_progress/semantics.ml
+++ examples/in_progress/semantics.ml
@@ -1,17 +1,18 @@
-type expr = Cte of int | Sub of expr * expr
+type expr = Cte of int | Sub of expr * expr | Add of expr * expr
 
 type prog = expr
 
 let [@logic] rec eval_0 = function
   | Cte n -> n
   | Sub (e1, e2) -> eval_0 e1 - eval_0 e2
+  | Add (e1, e2) -> eval_0 e1 + eval_0 e2
 (*@ eval_0 e
       variant e *)
 
 let [@logic] interpret_0 p = eval_0 p
 
 module Stack = struct
-  type opcode = OPush of int | OSub
+  type opcode = OPush of int | OSub | OAdd
 
   type program = opcode list
   type stack = int list
@@ -23,14 +24,13 @@ module Stack = struct
 
   type result = Error | Res of stack
 
-  (* @ function step (s: stack) (o: opcode) : stack *)
-  (* @ axiom step_push: forall x s. step s (OPush x) = x :: s *)
-  (* @ axiom step_sub : forall x y s. step (x :: y :: s) OSub = (x - y) :: s *)
-
   let [@logic] step stack = function
     | OPush x -> Res (x :: stack)
-    | OSub -> match stack with
+    | OSub -> begin match stack with
       | x :: y :: r -> Res ((x - y) :: r)
+      | _ -> Error end
+    | OAdd -> match stack with
+      | x :: y :: r -> Res ((x + y) :: r)
       | _ -> Error
 
   let [@logic] rec star (s: stack) = function
@@ -48,10 +48,13 @@ module Stack = struct
     match p1 with
     | [] -> ()
     | OPush n :: r -> star_append r p2 (n :: s1) s2 s
-    | OSub    :: r ->
+    | OSub    :: r -> begin match s1 with
+        | [] | [_] -> assert false (* to reinforce this cannot happen *)
+        | x :: y :: sr -> star_append r p2 ((x - y) :: sr) s2 s end
+    | OAdd    :: r ->
         match s1 with
-        | [] | [_] -> assert false
-        | x :: y :: sr -> star_append r p2 ((x - y) :: sr) s2 s
+        | [] | [_] -> assert false (* to reinforce this cannot happen *)
+        | x :: y :: sr -> star_append r p2 ((x + y) :: sr) s2 s
   (*@ star_append p1 p2 s1 s2 s
         requires Res s = star s1 p1
         variant  p1
@@ -65,7 +68,13 @@ module Compile = struct
 
   let rec compile = function
     | Cte n -> [Stack.OPush n]
-    | Sub (e1, e2) -> compile e2 @ compile e1 @ [OSub]
+    | Sub (e1, e2) ->
+        let a2 = compile e2 in
+        let a1 = compile e1 in
+        Stack.star_append a2 (a1 @ [OSub]) [] [] [eval_0 e2];
+        Stack.star_append a1 [OSub] [] [eval_0 e1] [eval_0 e1];
+        a2 @ a1 @ [OSub]
+    | Add (e1, e2) -> compile e2 @ compile e1 @ [OAdd]
   (*@ r = compile e
         variant e
         ensures star [] r = Res ((eval_0 e) :: []) *)
@@ -76,6 +85,8 @@ let rec eval_1 e k =
   | Cte n -> k n
   | Sub (e1, e2) ->
       eval_1 e1 (fun v1 -> eval_1 e2 (fun v2 -> k (v1 - v2)))
+  | Add (e1, e2) ->
+      eval_1 e1 (fun v1 -> eval_1 e2 (fun v2 -> k (v1 + v2)))
 (*@ r = eval_1 e k
       variant e
       ensures r = k (eval_0 e) *)
@@ -90,7 +101,12 @@ module Error = struct
 
   let [@logic] rec eval_0 = function
     | Cte n -> if n >= 0 then Vnum n else Underflow
-    | Sub (e1, e2) -> match eval_0 e1 with
+    | Sub (e1, e2) -> begin match eval_0 e1 with
+      | Underflow -> Underflow
+      | Vnum v1 -> begin match eval_0 e2 with
+        | Underflow -> Underflow
+        | Vnum v2 -> if v1 >= v2 then Vnum (v1 - v2) else Underflow end end
+    | Add (e1, e2) -> match eval_0 e1 with
       | Underflow -> Underflow
       | Vnum v1 -> match eval_0 e2 with
         | Underflow -> Underflow
