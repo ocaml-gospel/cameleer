d850cbab123a432672bc849d980d90a65a0d621b
N3C0 <carlosfranciscopintoscp@gmail.com>
2021-03-04 03:23:44 +0000

    BinTreeModule->Set approx

--- examples_carlos/BinTreeModule.ml
+++ examples_carlos/BinTreeModule.ml
@@ -21,7 +21,7 @@ module BinTree (TreeNode : Comparable) =
   struct
     type elt = TreeNode.t
     type t = |Node of t * elt * t
-            |Empty
+             |Empty
             
  
   (*@ function occ (x: elt) (t: t) : int = 
@@ -37,14 +37,14 @@ module BinTree (TreeNode : Comparable) =
     (*@ occ_nonneg x t
       ensures 0 <= (occ x t) *)
     
-    (*@ predicate mem (x: elt) (t: t) = occ x t > 0 *)
+    (*@ predicate mem2 (x: elt) (t: t) = occ x t > 0 *)
     
     
     (*@ predicate binsearchtree (t: t) = match t with
       | Empty -> true
       | Node l v r ->
-        (forall x. mem x l -> TreeNode.compare x v < 0) &&
-        (forall y. mem y r -> TreeNode.compare y v > 0) &&
+        (forall x. mem2 x l -> TreeNode.compare x v < 0) &&
+        (forall y. mem2 y r -> TreeNode.compare y v > 0) &&
          binsearchtree l && binsearchtree r *)
     
 
@@ -60,26 +60,44 @@ module BinTree (TreeNode : Comparable) =
         requires binsearchtree t
         ensures (occ x t) <= 1*)
         
+        
+    let create l v r =
+      (*let hl = match l with Empty -> 0 | Node {h} -> h in
+      let hr = match r with Empty -> 0 | Node {h} -> h in*)
+      (*Node{l; v; r; h=(if hl >= hr then hl + 1 else hr + 1)}*)
+      Node (l,v,r)
+    (*@ res = create l v r
+        requires binsearchtree l
+        requires binsearchtree r
+        requires not (mem2 v l || mem2 v r)
+        requires forall j:elt. (mem2 j l -> not mem2 j r) && (mem2 j r -> not mem2 j l)
+        requires forall y: elt. mem2 y l -> TreeNode.compare y v < 0
+        requires forall x: elt. mem2 x r -> TreeNode.compare x v > 0
+        ensures binsearchtree res
+        ensures forall z: elt. mem2 z l || mem2 z r || TreeNode.compare z v = 0 -> mem2 z res
+        ensures forall w: elt. if w <> v then occ w res = occ w l + occ w r else mem2 v res*)
+      
 
-    let rec find tree x =
+
+    let rec mem tree x =
       match tree with 
         |Node (l,a,r) -> let z = TreeNode.compare x a in
                       if (z=0) then true else
-                      find (if z < 0 then (l) else (r)) x
+                      mem (if z < 0 then (l) else (r)) x
         |Empty -> false
         (*@ r = find t x
         requires binsearchtree t
         variant t
-        ensures r <-> mem x t*)
+        ensures r <-> mem2 x t*)
 
 
     let rec insert tree x =
       match tree with
         |Empty -> Node (Empty,x,Empty)
-        |Node (l,y,r)-> let z = TreeNode.compare x y in
-                        if z < 0 then Node ((insert l x),y,r) else
-                        if z > 0 then Node (l, y, (insert r x))
-                        else Node (l,y,r)
+        |Node (l,y,r) as t-> let z = TreeNode.compare x y in
+                        if z < 0 then create (insert l x) y r else
+                        if z > 0 then create l y (insert r x)
+                        else t
     (*@r = insert t x
       requires binsearchtree t
       variant t
@@ -88,18 +106,30 @@ module BinTree (TreeNode : Comparable) =
       ensures binsearchtree r *)
     
     (*@ predicate is_minimum (x: elt) (tree: t) =
-      mem x tree /\ forall e: elt. mem e tree -> TreeNode.compare x e < 0 \/ e = x *)
+      mem2 x tree /\ forall e: elt. mem2 e tree -> TreeNode.compare x e < 0 \/ e = x *)
+      
+    (*@ predicate is_maximum (x:elt) (tree: t) =
+      mem2 x tree /\ forall e: elt. mem2 e tree -> TreeNode.compare x e > 0 \/ e = x*)
     
     let [@logic] rec min_tree (tree: t) : elt =
       match tree with
         | Empty -> assert false
         | Node (Empty, a, _) -> a
         | Node (l, _, _) -> min_tree l
-    (*@ r = min t
+    (*@ r = min_tree t
       variant t
       requires t <> Empty && binsearchtree t
       ensures is_minimum r t*)
-
+    
+    let [@logic] rec max_tree (tree: t) : elt =
+      match tree with
+        | Empty -> assert false
+        | Node (_, a, Empty) -> a
+        | Node (_,_, r) -> max_tree r
+    (*@ r = max_tree t
+      variant t
+      requires t <> Empty && binsearchtree t
+      ensures is_maximum r t*)
 
     let [@lemma] rec is_minimum_min (t: t) =
       match t with
@@ -111,10 +141,47 @@ module BinTree (TreeNode : Comparable) =
       requires binsearchtree t
       variant t
       ensures is_minimum (min_tree t) t*)
+      
+    let [@lemma] rec is_maximum_max (t: t) = 
+      match t with 
+        |Empty -> assert false
+        |Node (_, _, Empty) -> ()
+        |Node (_, _, r) -> is_maximum_max r
+    (*@ is_maximum_max t
+      requires t <> Empty
+      requires binsearchtree t
+      variant t
+      ensures is_maximum (max_tree t) t*)
 
 
+          
+    let rec remove_min_elt = function
+        Empty -> assert false 
+      | Node (Empty, _ , r) -> r
+      | Node(l, v, r) -> create (remove_min_elt l) v r
+    (*@ r = remove_min_elt param
+      variant param
+      requires binsearchtree param
+      requires param <> Empty
+      ensures binsearchtree r
+      ensures forall j: elt. TreeNode.compare j (min_tree param) <> 0 -> occ j param = occ j r
+      ensures not mem2 (min_tree param) r*)
 
-    let rec find_min tree =
+
+      
+    let merge t1 t2 =
+      match (t1, t2) with
+        (Empty, t) -> t
+        | (t, Empty) -> t
+        | (_, _) -> create t1 (min_tree t2) (remove_min_elt t2)
+    (*@ r = merge t1 t2 
+      requires binsearchtree t1 && binsearchtree t2
+      requires forall j: elt. mem2 j t1 -> (forall y: elt. mem2 y t2 -> TreeNode.compare j y < 0)
+      requires forall j:elt. (mem2 j t1 -> not mem2 j t2) && (mem2 j t2 -> not mem2 j t1)
+      ensures forall j: elt. occ j t1 + occ j t2 = occ j r
+      ensures binsearchtree r*)
+ 
+    (*let rec find_min tree =
       match tree with
         | Empty -> assert false
         | Node (Empty, x, r) -> (x, r)
@@ -126,20 +193,17 @@ module BinTree (TreeNode : Comparable) =
       ensures binsearchtree x
       ensures forall j: elt. j <> r -> occ j t = occ j x
       ensures occ r x = (occ r t) - 1
-      ensures r = min_tree t*)
+      ensures r = min_tree t*)*)
     
     
-    let rec delete tree x =
+    let rec remove tree x =
       match tree with
         |Empty -> Empty
         |Node (l,a,r) -> let z = TreeNode.compare x a in
-                        if z < 0 then Node ((delete l x), a, r) else
-                        if z > 0 then Node (l, a, (delete r x)) else 
-                        match l, r with
-                          |Empty, _ -> r
-                          |_, Empty -> l
-                          |l', r' -> let (min, x') = find_min r' in Node (l', min, (x'))
-    (*@ r = delete t x
+                        if z < 0 then create (remove l x) a r else
+                        if z > 0 then create l a (remove r x) else 
+                        begin assert (x = a);merge l r end
+    (*@ r = remove t x
       requires binsearchtree t
       variant t
       ensures forall j: elt. (x<>j) -> occ j t = occ j r
