f265b465bdb09c7c69436aff7486a02495ebcfa0
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-04-20 19:46:28 +0100

    Plugin entry-poit: back to Why3 support

--- src/plugin/plugin_cameleer.ml
+++ src/plugin/plugin_cameleer.ml
@@ -147,7 +147,7 @@ let read_channel env path file c =
     | x :: r -> Format.eprintf "%a" pp x; pp_list pp fmt r in
   let rec pp_decl fmt d = match d with
     | Odecl.Odecl (_loc, d) ->
-        Format.fprintf fmt "%a@." (Mlw_printer.pp_decl ~attr:false) d
+        Format.fprintf fmt "%a@." Mlw_printer.pp_decl d
     | Odecl.Omodule (_loc, id, dl) ->
         Format.eprintf "@[<hv 2>scope %s@\n%a@]@\nend@." id.id_str
           (pp_list pp_decl) dl in
