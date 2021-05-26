0948c1812fe3e46f22770ebd8627cf191e62e93b
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-05-26 19:18:55 +0100

    New example: max element in an array of integers.
    The code and specification follow the 1st challenge of the
    FoVeOOs 2011 competition.

---
+++ examples/max_elt_array.ml
@@ -0,0 +1,15 @@
+let max a =
+  let x = ref 0 in
+  let y = ref (Array.length a - 1) in
+  while not (!x = !y) do
+    (*@ variant   !y - !x
+        invariant 0 <= !x <= !y < Array.length a
+        invariant forall i. (0 <= i < !x \/ !y < i < Array.length a) ->
+                            (a.(i) <= a.(!y) \/ a.(i) <= a.(!x)) *)
+    if a.(!x) <= a.(!y) then incr x else decr y
+  done;
+  !x
+(*@ r = max a
+      requires Array.length a > 0
+      ensures  0 <= r < Array.length a
+      ensures  forall i. 0 <= i < Array.length a -> a.(i) <= a.(r) *)
