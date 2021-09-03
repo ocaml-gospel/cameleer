31dddc01781626373740707988ee0e7d8b56dc7c
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-09-03 11:15:09 +0100

    Insertion sort: sorted predicate recursively-defined.

--- examples/insertion_sort_list.ml
+++ examples/insertion_sort_list.ml
@@ -15,29 +15,25 @@ module type PRE_ORD = sig
   val leq : t -> t -> bool
   (*@ b = leq x y
         ensures b <-> le x y *)
-
-  (*@ predicate sorted_list (l: t list) *)
-  (*@ axiom sorted_nil: sorted_list [] *)
-  (*@ axiom sorted_singleton: forall x. sorted_list (x :: []) *)
-  (*@ axiom sorted_cons: forall x y l.
-        le x y -> sorted_list (y :: l) -> sorted_list (x :: (y :: l)) *)
-  (*@ axiom sorted_list_inversion: forall l.
-        sorted_list l ->
-          (l = []) \/ (exists x. l = x :: []) \/
-          (exists x y ll. le x y /\ sorted_list (y :: ll) /\ l = x :: y :: l) *)
 end
 
 module InsertionSort (E: PRE_ORD) = struct
 
   type elt = E.t
 
+  let[@logic] rec sorted_list = function
+    | [] | [_] -> true
+    | x :: (y :: r) -> E.leq x y && sorted_list (y :: r)
+  (*@ sorted_list l
+        variant l *)
+
   (*@ lemma sorted_mem: forall x l.
-        (forall y. List.mem y l -> le x y) /\ sorted_list l <->
+        (forall y. List.mem y l -> E.le x y) /\ sorted_list l <->
         sorted_list (x :: l) *)
 
   (*@ lemma sorted_append: forall l1 l2.
         (sorted_list l1 && sorted_list l2 &&
-          (forall x y. List.mem x l1 -> List.mem y l2 -> le x y)) <->
+          (forall x y. List.mem x l1 -> List.mem y l2 -> E.le x y)) <->
         sorted_list (l1 ++ l2) *)
 
   let rec insert x = function
