3512e76051074639ee5df9a33d0d3a7443c666d2
Mário Pereira <mjp.pereira@fct.unl.pt>
2022-05-17 13:04:47 +0100

    Small example of operational semantics using inductives (wip)

--- examples/inductive_predicates.ml
+++ examples/inductive_predicates.ml
@@ -16,15 +16,28 @@ struct
 
   (*@ inductive sems_arith (a: arith) (s: store) (n: int) =
       | const: forall s n. sems_arith (AConst n) s n
-      | var: forall s x. sems_arith (Avar x) s (s x)
-      | mul: forall s a1 a1 n1 n2.
+      | var: forall s x. sems_arith (AVar x) s (s x)
+      | mul: forall s: store, a1: arith, a2: arith, n1 n2.
              sems_arith a1 s n1 -> sems_arith a2 s n2 ->
-             sems_arith (AMul (a1, a2)) s (n1 * n2)
-      | plus: forall s a1 a1 n1 n2.
+             sems_arith (AMul a1 a2) s (n1 * n2)
+      | plus: forall s a1 a2 n1 n2.
              sems_arith a1 s n1 -> sems_arith a2 s n2 ->
-             sems_arith (APlus (a1, a2)) s (n1 + n2)
-      | minus: forall s a1 a1 n1 n2.
+             sems_arith (APlus a1 a2) s (n1 + n2)
+      | minus: forall s a1 a2 n1 n2.
              sems_arith a1 s n1 -> sems_arith a2 s n2 ->
-             sems_arith (Minus (a1, a2)) s (n1 - n2) *)
+             sems_arith (AMinus a1 a2) s (n1 - n2) *)
+
+  let rec interpreter store = function
+    | AConst n -> n
+    | AVar x -> store x
+    | AMul (a1, a2) ->
+        interpreter store a1 * interpreter store a2
+    | APlus (a1, a2) ->
+        interpreter store a1 + interpreter store a2
+    | AMinus (a1, a2) ->
+        interpreter store a1 - interpreter store a2
+  (*@ r = interpreter s a
+        variant a
+        ensures sems_arith a s r *)
 
 end
