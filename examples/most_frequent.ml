435c4ffda5411a722ad6adaed095ca710c1bee65
Mário Pereira <mjp.pereira@fct.unl.pt>
2025-10-08 16:37:46 +0100

    Most frequent element in a sorted array: functorial version

--- examples/most_frequent.ml
+++ examples/most_frequent.ml
@@ -27,24 +27,51 @@
 (*@ function numof_eq (a: 'a array) (v: 'a) (l u: integer) : integer =
       numof (fun i -> a.(i) = v) l u *)
 
-let most_frequent (a: int array) : int
-= let r = ref a.(0) in
-  let c = ref 1 in
-  let m = ref 1 in
-  for i = 1 to Array.length a - 1 do
-    (*@ invariant !c = numof_eq a a.(i-1) 0 i
-        invariant !m = numof_eq a !r 0 i
-        invariant forall x. numof_eq a x 0 i <= !m *)
-    if a.(i) = a.(i-1) then begin
-      incr c;
-      if !c > !m then begin m := !c; r := a.(i) end
-    end else
-      c := 1
-  done;
-  !r
-(*@ r = most_frequent a
-    requires Array.length a > 0
-    requires forall i j. 0 <= i <= j < Array.length a -> a[i] <= a[j]
-    ensures  numof_eq a r 0 (Array.length a) > 0
-    ensures  forall x. numof_eq a x 0 (Array.length a) <= numof_eq a r 0 (Array.length a)
-*)
+module type COMP = sig
+  type t
+
+  val eq : t -> t -> bool
+  (*@ b = eq x y
+        ensures b <-> x = y *)
+
+  (*@ function le: t -> t -> bool *)
+
+  (*@ axiom reflexive : forall x. le x x *)
+  (*@ axiom total     : forall x y. le x y \/ le y x *)
+  (*@ axiom transitive: forall x y z. le x y -> le y z -> le x z *)
+
+  val leq : t -> t -> bool
+  (*@ b = leq x y
+        ensures b <-> le x y *)
+
+end
+
+module Make (C: COMP) = struct
+  type elt = C.t
+
+  (*@ lemma not_le_eq : forall x y: elt. C.le y x -> not y = x ->
+        forall w: elt. C.le w y -> not w = x *)
+
+  let most_frequent (a: elt array) : elt
+  = let r = ref a.(0) in
+    let c = ref 1 in
+    let m = ref 1 in
+    for i = 1 to Array.length a - 1 do
+      (*@ invariant !c = numof_eq a a.(i-1) 0 i
+          invariant !m = numof_eq a !r 0 i
+          invariant forall x. numof_eq a x 0 i <= !m *)
+      if C.eq a.(i) a.(i-1) then begin
+        incr c;
+        if !c > !m then begin m := !c; r := a.(i) end
+      end else
+        c := 1
+    done;
+    !r
+    (*@ r = most_frequent a
+        requires Array.length a > 0
+        requires forall i j. 0 <= i <= j < Array.length a -> C.le a[i] a[j]
+        ensures  numof_eq a r 0 (Array.length a) > 0
+        ensures  forall x. numof_eq a x 0 (Array.length a) <= numof_eq a r 0 (Array.length a)
+    *)
+
+end
