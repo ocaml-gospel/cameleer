6a5a644fd67cfc11133a12f651846dc3a28f4ff3
Paul Patault <paul.patault@lmf.cnrs.fr>
2026-03-13 16:20:01 +0000

    example: non polymorphic types

--- skew_heaps.ml
+++ skew_heaps.ml
@@ -1,38 +1,38 @@
 type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
 
-let empty: 'a tree = Empty
+let empty: int tree = Empty
 
-let le a b = true (* TODO *)
+(* let le a b = true (* TODO *) *)
 
-let is_empty (t: 'a tree) : bool =
+let is_empty (t: int tree) : bool =
   match t with
   | Empty -> true
-  | Node ((l: 'a tree), (x: 'a), (r: 'a tree)) -> false
+  | Node ((l: int tree), (x: int), (r: int tree)) -> false
 (*@ r = is_empty t
       ensures r <-> t = Empty *)
 
-let rec merge (t1: 'a tree) (t2: 'a tree) : 'a tree =
+let rec merge (t1: int tree) (t2: int tree) : int tree =
     match t1, t2 with
-    | Empty, (_: 'a tree) -> t2
-    | (_: 'a tree), Empty -> t1
-    | Node ((l1: 'a tree), (x1: 'a), (r1: 'a tree)),
-      Node ((l2: 'a tree), (x2: 'a), (r2: 'a tree)) ->
-       if (le x1 x2 : bool) then
+    | Empty, (_: int tree) -> t2
+    | (_: int tree), Empty -> t1
+    | Node ((l1: int tree), (x1: int), (r1: int tree)),
+      Node ((l2: int tree), (x2: int), (r2: int tree)) ->
+       if x1 < x2 then
          let l = merge r1 t2 in
          Node (l, x1, l1)
        else
          let l = merge r2 t1 in
          Node (l, x2, l2)
 
-let add (x: 'a) (t: 'a tree) : 'a tree =
+let add (x: int) (t: int tree) : int tree =
   merge (Node (Empty, x, Empty)) t
 
-let remove_min (t: 'a tree) : 'a tree =
+let remove_min (t: int tree) : int tree =
   match t with
   | Empty      -> assert false
-  | Node ((l: 'a tree), (_: 'a), (r: 'a tree)) -> merge l r
+  | Node ((l: int tree), (_: int), (r: int tree)) -> merge l r
 
-let get_min (t: 'a tree) : 'a =
+let get_min (t: int tree) : int =
   match t with
   | Empty      -> assert false
-  | Node ((_: 'a tree), (x: 'a), (_: 'a tree)) -> x
+  | Node ((_: int tree), (x: int), (_: int tree)) -> x
