4054f1ff0e5db5d15baab20adbbda5b402c468cd
Mário Pereira <mjp.pereira@fct.unl.pt>
2026-03-14 03:38:02 +0000

    Tentative Gospel spec for Skew Heaps
      - For the moment, this spec does not type-check.
    
      - We are lacky every single definition.
    
      - It is just to exercise our translation schema.

--- skew_heaps.ml
+++ skew_heaps.ml
@@ -23,16 +23,39 @@ let rec merge (t1: int tree) (t2: int tree) : int tree =
        else
          let l = merge r2 t1 in
          Node (l, x2, l2)
+(*@ r = merge t1 t2
+      requires heap t1
+      requires heap t2
+      ensures  heap r
+      ensures  forall x. occ x r = occ x t1 + occ x t2
+      ensures  size r = size t1 + size t2 *)
 
 let add (x: int) (t: int tree) : int tree =
   merge (Node (Empty, x, Empty)) t
+(*@ r = add x t
+      requires heap t
+      ensures  heap r
+      ensures  size r = size t + 1
+      ensures  occ x r = occ x t + 1
+      ensures  forall y. y <> x -> occ y r = occ y t *)
 
 let remove_min (t: int tree) : int tree =
   match t with
   | Empty      -> assert false
   | Node ((l: int tree), (_: int), (r: int tree)) -> merge l r
+(*@ r = remove_min t
+      requires heap t
+      requires size t > 0
+      ensures  heap r
+      ensures  occ (minimum t) r = occ (minimum t) r - 1
+      ensures  forall e. e <> minimum t -> occ e r = occ e t
+      ensures  size r = size t - 1 *)
 
 let get_min (t: int tree) : int =
   match t with
   | Empty      -> assert false
   | Node ((_: int tree), (x: int), (_: int tree)) -> x
+(*@ r = get_min t
+      requires heap t
+      requires size t > 0
+      ensures  r = minimum t *)
