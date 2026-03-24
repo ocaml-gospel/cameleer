b573854461f745756ac37db77188b05b56c1cba3
Paul Patault <paul.patault@lmf.cnrs.fr>
2026-03-24 10:31:55 +0000

    test script: minor changes

--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-dune clean
-dune build
+RESET="\033[0m"
+RED="\033[31m"
+GREEN="\033[32m"
 
-for file in "test"/*"ml"; do
+WHY3="${WHY3:-${HOME}/git/why3/bin/why3}"
+
+if [ ! -e ${WHY3} ]; then
+    printf "${RED}%s${RESET}\n" "${WHY3} is not a valid why3"
+    exit 1
+fi
+
+files="${1:-test/*.ml}"
+
+for file in ${files}; do
     [ -e "${file}" ] || continue
 
     name="${file%.*}.coma"
 
-    dune exec bin/cli_coma.exe -- --coma --pat "${file}" &> /dev/null
+    dune exec bin/cli_coma.exe -- --coma --pat "${file}" >/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
         if [ -e "${name}" ]; then
-            printf '\033[32m%s\033[0m\n' "ok: ${name}"
+            printf "${GREEN}%s${RESET}\n" "ok: ${name}"
+            if "${WHY3}" prove "./${name}" > /dev/null 2>&1; then
+                printf "${GREEN}%s${RESET}\n" "    why3 prove succeed"
+            else
+                printf "${RED}%s${RESET}\n" "    why3 prove failed"
+            fi
         else
             echo "error with the script"
             exit 1
         fi
     else
-        printf '\033[31m%s\033[0m\n' "ko: ${file}"
+        printf "${RED}%s${RESET}\n" "ko: ${file}"
     fi
 done
