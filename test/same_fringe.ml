7de83f88cf8569a48b06190a0155d452098fd84d
Beatriz Rosas <b.rosas@campus.fct.unl.pt>
2026-03-31 14:48:11 +0100

    Added missing return types.

--- test/same_fringe.ml
+++ test/same_fringe.ml
@@ -12,7 +12,7 @@ type enum = (elt * elt tree) list
       | [] -> []
       | (x, r) :: e -> x :: (elements r @ enum_elements e) *)
 
-let rec mk_zipper (t : elt tree) (e : enum) =
+let rec mk_zipper (t : elt tree) (e : enum) : enum =
   match (t : elt tree) with
   | Empty -> e
   | Node ((l: elt tree), (x: elt), (r: elt tree)) -> mk_zipper l ((x, r) :: e)
@@ -21,13 +21,13 @@ let rec mk_zipper (t : elt tree) (e : enum) =
       requires true
       ensures enum_elements r = elements t @ enum_elements e *)
 
-let rec eq_enum (e1 : enum) (e2 : enum) =
+let rec eq_enum (e1 : enum) (e2 : enum) : bool =
   match ((e1 : enum), (e2 : enum)) with
   | [], [] -> true
   | ((x1: elt), (r1:elt tree)) :: (e1: enum), ((x2:elt), (r2: elt tree)) :: (e2: enum) ->
       if x1 = x2 then
-        let e1 = mk_zipper r1 e1 in
-        let e2 = mk_zipper r2 e2 in
+        let (e1: enum) = mk_zipper r1 e1 in
+        let (e2: enum) = mk_zipper r2 e2 in
         eq_enum e1 e2
       else false
       (* x1 = x2 &&
@@ -41,7 +41,7 @@ let rec eq_enum (e1 : enum) (e2 : enum) =
       requires true
       ensures b <-> enum_elements e1 = enum_elements e2 *)
 
-let same_fringe (t1 : elt tree) (t2 : elt tree) =
+let same_fringe (t1 : elt tree) (t2 : elt tree) : bool =
   let e1 = mk_zipper t1 [] in
   let e2 = mk_zipper t2 [] in
   eq_enum e1 e2
