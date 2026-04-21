32e37550d2efdfbe1e14c92af22da1eb455d39b6
Beatriz Rosas <b.rosas@campus.fct.unl.pt>
2026-04-21 10:36:46 +0100

    Same_fringe example (cameleer can't prove the top level spec tho).

--- test/same_fringe_new_indepth.ml
+++ test/same_fringe_new_indepth.ml
@@ -14,16 +14,18 @@ let rec mk_zipper (t : elt tree) (e : enum) : enum =
   match (t : elt tree) with
   | Empty -> e
   | Node ((l: elt tree), (x: elt), (r: elt tree)) 
-  -> mk_zipper l (Next (x, r, e))
+  [@gospel {| requires true
+             ensures enum_elements result = elements t @ enum_elements e |}] 
+      -> mk_zipper l (Next (x, r, e))
 (*@ r = mk_zipper t e
-      requires true
-      ensures enum_elements r = elements t @ enum_elements e
       variant t *)
 
 let rec eq_enum (e1 : enum) (e2 : enum) : bool =
   match ((e1 : enum), (e2 : enum)) with
   | Done, Done -> true
-  | (Next ((x1: elt), (r1:elt tree), (e11: enum)), Next ((x2:elt), (r2: elt tree),(e22: enum))) ->
+  | (Next ((x1: elt), (r1:elt tree), (e11: enum)), Next ((x2:elt), (r2: elt tree),(e22: enum))) 
+  [@gospel {| requires true
+              ensures result <-> (enum_elements e1 = enum_elements e2) |}] ->
       if x1 = x2 then
         let (e13: enum) = mk_zipper r1 e11 in
         let (e23: enum) = mk_zipper r2 e22 in
@@ -31,14 +33,13 @@ let rec eq_enum (e1 : enum) (e2 : enum) : bool =
       else false
   | (_: enum), (_: enum) -> false
 (*@ b = eq_enum e1 e2
-      requires true
-      ensures b <-> (enum_elements e1 = enum_elements e2)
       variant List.length (enum_elements e1) *)
 
 let same_fringe (t1 : elt tree) (t2 : elt tree) : bool =
   let (e1: enum) = mk_zipper t1 Done in
   let (e2: enum) = mk_zipper t2 Done in
   eq_enum e1 e2
-(*@ b = same_fringe t1 t2 
-      requires true
+(* @ b = same_fringe t1 t2
+      requires true 
       ensures b <-> elements t1 = elements t2 *)
+      
